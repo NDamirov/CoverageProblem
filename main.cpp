976645e97ecc5b6a3d2dcb6b9c5c144c34d074ee
Namig Damirov <namig.damirov@gmail.com>
2023-02-01 00:29:22 +0300

    Path decomposition added

--- main.cpp
+++ main.cpp
@@ -1,18 +1,25 @@
 #include <vector>
 #include <string>
-#include "or-tools/include/ortools/graph/assignment.h"
+#include <iostream>
+#include <fstream>
+
+#include "ortools/linear_solver/linear_solver.h"
+using namespace operations_research;
+using namespace std;
 
 template<typename FieldElement>
 class Field {
 public:
     Field<int>(std::vector<std::string> in) {
-        Resize(in.size(), in[0].size());
+        Resize(in[0].size(), in.size());
+        spaces_ = 0;
         for (int i = 0; i < in.size(); ++i) {
             for (int j = 0; j < in[i].size(); ++j) {
                 if (in[i][j] == '*') {
                     field_[i][j] = 1;
+                    ++spaces_;
                 } else {
-                    field_[i][j] = 0;
+                    field_[i][j] = -1;
                 }
             }
         }
@@ -32,45 +39,89 @@ public:
         field_.resize(h, std::vector<FieldElement>(w));
     }
 
-    std::vector<std::vector<int>> CreateMILPH() {
-        int n = width_ * height_;
-        std::vector<std::vector<int>> coefs(n, std::vector<int>(n));
+    std::vector<std::vector<FieldElement>> Solve() {
+        std::vector<double> obj_coefs(3 * spaces_, 0);
+        for (int i = 0; i < 2 * spaces_; ++i) {
+            obj_coefs[i] = -1;
+        }
 
-        for (int i = 0; i < width_; ++i) {
-            for (int j = 0; j < height_; ++j) {
-                int ind = i * width_ + j;
-                int left = i * width_ + (j - 1);
-                coefs[ind][ind] = 1;
-                coefs[ind][left] = -1;
+        std::vector<double> bounds(2 * spaces_, 0);
+        std::vector<std::vector<double>> coefs(2 * spaces_, std::vector<double>(3 * spaces_, 0));
+
+        int ind = 0;
+        int c_ind = 0;
+        LOG(INFO) << "Building coefs";
+        for (int i = 0; i < height_; i++) {
+            for (int j = 0; j < width_; ++j){ 
+                if (field_[i][j] == -1) {
+                    continue;
+                }
+                field_[i][j] = ind;
+                //------------
+                coefs[c_ind][ind] = coefs[c_ind + 1][spaces_ + ind] = -1; // y_h[i], y_v[i]
+                coefs[c_ind][spaces_ + spaces_ + ind] = 1; // x_h[i]
+                if (!(j == 0 || field_[i][j - 1] == -1)) {
+                    coefs[c_ind][spaces_ + spaces_ + ind - 1] = -1; // x_h[left(i)]
+                }
+                //------------
+                coefs[c_ind + 1][spaces_ + spaces_ + ind] = -1; // x_h[i]
+                bounds[c_ind + 1] = -1;
+                if (!(i == 0 || field_[i - 1][j] == -1)) {
+                    bounds[c_ind + 1] = 0;
+                    coefs[c_ind + 1][spaces_ + spaces_ + field_[i - 1][j]] = 1;
+                }
+                ++ind;
+                c_ind += 2;
             }
         }
 
-        return coefs;
-    }
+        LOG(INFO) << "Starting solver";
+        std::unique_ptr<MPSolver> solver(MPSolver::CreateSolver("SCIP"));
+        if (!solver) {
+            LOG(ERROR) << "SCIP solver unavailable.";
+            return {};
+        }
 
-    std::vector<std::vector<int>> CreateMILPV() {
-        int n = width_ * height_;
-        std::vector<std::vector<int>> coefs(n, std::vector<int>(n));
-
-        for (int i = 0; i < width_; ++i) {
-            for (int j = 0; j < height_; ++j) {
-                int ind = i * width_ + j;
-                int up = (i - 1) * width_ + j;
-                coefs[ind][ind] = 1;
-                coefs[ind][up] = -1;
+        std::vector<const MPVariable*> x(3 * spaces_);
+        for (int j = 0; j < 3 * spaces_; ++j) {
+            x[j] = solver->MakeIntVar(0.0, 1.0, "");
+        }
+        
+        for (int i = 0; i < 2 * spaces_; ++i) {
+            MPConstraint* constraint = solver->MakeRowConstraint(-solver->infinity(), bounds[i], "");
+            for (int j = 0; j < 3 * spaces_; ++j) {
+                constraint->SetCoefficient(x[j], coefs[i][j]);
             }
         }
 
-        return coefs;
+        MPObjective* const objective = solver->MutableObjective();
+        for (int j = 0; j < 3 * spaces_; ++j) {
+            objective->SetCoefficient(x[j], obj_coefs[j]);
+        }
+        objective->SetMaximization();
+
+        const MPSolver::ResultStatus result_status = solver->Solve();
+
+        ind = 2 * spaces_;
+        for (int i = 0; i < height_; ++i) {
+            for (int j = 0; j < width_; ++j) {
+                if (field_[i][j] == -1) {
+                    field_[i][j] = -1;
+                    continue;
+                }
+                field_[i][j] = x[ind++]->solution_value();
+            }
+        }
+        return field_;
     }
 
 private:
     int width_;
     int height_;
+    int spaces_;
     std::vector<std::vector<FieldElement>> field_;
+    std::vector<std::vector<FieldElement>> sol_;
 };
-
-#include <fstream>
 
 Field<int> read_field(const std::string& file_name) {
     std::ifstream fin(file_name);
@@ -87,6 +138,19 @@ Field<int> read_field(const std::string& file_name) {
 
 int main() { 
     Field<int> current = read_field("field.txt");
+    auto result = current.Solve();
+    for (const std::vector<int>& x : result) {
+        for (int y : x) {
+            if (y == -1) {
+                cout << "*";
+            } else if (y == 0) {
+                cout << "|";
+            } else {
+                cout << "-";
+            }
+        }
+        cout << "\n";
+    }
     return 0;
 }
 
